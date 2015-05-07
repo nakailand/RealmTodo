329c7187aad986b4e8d539cd01593474be1fec45
nakazy <nakazy@nakazy-no-iMac.local>
2015-05-07 23:09:28 +0900

    Initial Commit

---
+++ RealmToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmToDo
+//
+//  Created by nakazy on 2015/05/07.
+//  Copyright (c) 2015年 nakazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
